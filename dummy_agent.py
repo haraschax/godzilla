9eff8083fe85021b57e36e6b0cfddab2cb473400
Harald Schaefer <harald.the.engineer@gmail.com>
2023-12-19 03:17:25 -0800

    Dummy agent

---
+++ dummy_agent.py
@@ -0,0 +1,12 @@
+import random
+from constants import PlayerState, DIESIDE
+from typing import List
+
+class PlayerStrategy:
+  def yield_tokyo(self, me: PlayerState, other_player: PlayerState):
+    # TODO implement smart logic
+    return True
+    
+  def keep_dice(self, me: PlayerState, other_player: PlayerState, dice: List[DIESIDE], reroll_n: int):
+    # TODO implement smart logic
+    return [True for _ in range(len(dice))]
